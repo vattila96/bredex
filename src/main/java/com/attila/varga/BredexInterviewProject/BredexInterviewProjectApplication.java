84eb1344f4f84b00b634106e755de13211a4738e
Attila Varga <srep.ati@gmail.com>
2021-10-26 12:01:01 +0200

    add init files

---
+++ src/main/java/com/attila/varga/BredexInterviewProject/BredexInterviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.attila.varga.BredexInterviewProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BredexInterviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BredexInterviewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attila/varga/BredexInterviewProject/BredexInterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attila.varga.BredexInterviewProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BredexInterviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
